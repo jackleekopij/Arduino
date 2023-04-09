4b2c15161f34ceb72bfb79e9ae45758695b9144f
jackleekopij <jack.a.leekopij@gmail.com>
2023-04-09 09:09:35 -0500

    added comments to define variables in Kalman filter equation

--- Self Balancing Car/KalmanFilter.cpp
+++ Self Balancing Car/KalmanFilter.cpp
@@ -5,27 +5,40 @@ void KalmanFilter::Yiorderfilter(float angle_m, float gyro_m,float dt,float K1) 
 }
 
 void KalmanFilter::Kalman_Filter(double angle_m, double gyro_m,float dt,float Q_angle,float Q_gyro,float R_angle,float C_0) {
+  // Kalman filter for quantifying the smoothed angle 
+  // 2-dimensional Kalman gain matrix K_0 and K_1
   angle += (gyro_m - q_bias) * dt;
+  // calculate angle error 
   angle_err = angle_m - angle;
+
   Pdot[0] = Q_angle - P[0][1] - P[1][0];
   Pdot[1] = - P[1][1];
   Pdot[2] = - P[1][1];
   Pdot[3] = Q_gyro;
+
+  // predicted error covariance
   P[0][0] += Pdot[0] * dt;
   P[0][1] += Pdot[1] * dt;
   P[1][0] += Pdot[2] * dt;
   P[1][1] += Pdot[3] * dt;
+  
+  // calculates the innovation covariance
   PCt_0 = C_0 * P[0][0];
   PCt_1 = C_0 * P[1][0];
-  E = R_angle + C_0 * PCt_0;
+  E = C_0 * PCt_0 + R_angle;
+
+  // calculating the Kalman gain (2d-vector)
+  // PH/S
   K_0 = PCt_0 / E;
   K_1 = PCt_1 / E;
   t_0 = PCt_0;
   t_1 = C_0 * P[0][1];
+
   P[0][0] -= K_0 * t_0;
   P[0][1] -= K_0 * t_1;
   P[1][0] -= K_1 * t_0;
   P[1][1] -= K_1 * t_1;
+
   angle += K_0 * angle_err;
   q_bias += K_1 * angle_err;
   angle_dot = gyro_m - q_bias;
